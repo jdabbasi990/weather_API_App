bc42c6c3af81255a63c005991df3b8249f160541
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-03-31 15:06:53 -0400

    Initial Commit

---
+++ weatherAPI/weatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAPIApp.swift
+//  weatherAPI
+//
+//  Created by Jawwad Abbasi on 2024-03-31.
+//
+
+import SwiftUI
+
+@main
+struct weatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
